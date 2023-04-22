7787f1a26237e326a56ee278ea99918ca7176074
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-22 14:32:50 -0500

    disabled ds1 and 2 buttons

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -2,8 +2,8 @@ function StepOne({choice}) {
 
     return(
         <div className="btn-group btn-group-vertical lg:btn-group-horizontal flex justify-center items-center h-screen pb-20">
-            <button className="btn" onClick={() => choice('Dark Souls')}>Dark Souls</button>
-            <button className="btn" onClick={() => choice('Dark Souls 2')}>Dark Souls 2</button>
+            <button className="btn btn-disabled" onClick={() => choice('Dark Souls')}>Dark Souls</button>
+            <button className="btn btn-disabled" onClick={() => choice('Dark Souls 2')}>Dark Souls 2</button>
             <button className="btn" onClick={() => choice('Dark Souls 3')}>Dark Souls 3</button>
         </div>
     )
