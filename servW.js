773c708e368f48fe08d4493ad861f4623e663e52
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-22 17:09:59 -0500

    plz

--- servW.js
+++ servW.js
@@ -2,13 +2,13 @@
 if ('serviceWorker' in navigator) {
     // Wait for the 'load' event to not block other work
     window.addEventListener('load', async () => {
-      // Try to register the service worker.
-      try {
+        // Try to register the service worker.
+        try {
         const reg = await navigator.serviceWorker.register('/sw.js')
-  
+
         console.log('Service worker registered! 😎', reg);
-      } catch (err) {
+        } catch (err) {
         console.log('😥 Service worker registration failed: ', err);
-      }
+        }
     });
-  }
+}
