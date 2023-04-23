0eb51024df0333665fa215c714f0db39a5c661e4
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-23 12:23:52 -0500

    removed btn for real

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
         {gameChoice == '' && <StepOne choice={setGameChoice} /> }
         {gameChoice == 'Dark Souls 3' && <DarkSoulsThree choice={setGameChoice} />}
-        <button id="butInstall" type="button" className="btn absolute bottom-5 left-5 text-xs hide">Install</button>
+        {/* <button id="butInstall" type="button" className="btn absolute bottom-5 left-5 text-xs hidden">Install</button> */}
     </div>
   )
 }
