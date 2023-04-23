3cca9465f9f55265e35a4c60fb1a4e8ce7bb7597
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-23 12:06:08 -0500

    instal handler

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,30 @@ import './App.css'
 function App() {
   const [gameChoice, setGameChoice] = useState('')
 
+  const installHandler = async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    document.getElementById('butInstall').classList.toggle('hidden', true)
+  }
+
   return (
     <div className="">
         {gameChoice == '' && <StepOne choice={setGameChoice} /> }
         {gameChoice == 'Dark Souls 3' && <DarkSoulsThree choice={setGameChoice} />}
-        <button id="butInstall" type="button" className="btn absolute bottom-5 left-5 text-xs">Install</button>
+        <button id="butInstall" onClick={installHandler} type="button" className="btn absolute bottom-5 left-5 text-xs">Install</button>
     </div>
   )
 }
