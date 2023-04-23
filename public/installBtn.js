1a8e7d15f87ea2be07468b6f759924a0dd6fa4bd
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-23 10:58:49 -0500

    asdf

--- public/installBtn.js
+++ public/installBtn.js
@@ -6,23 +6,23 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
     // show install button
     document.getElementById('butInstall').classList.toggle('hidden', false)
-})
 
-document.getElementById('butInstall').addEventListener('click', async () => {
-    console.log('👍', 'butInstall-clicked');
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-      // The deferred prompt isn't available.
-      return;
-    }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
-    const result = await promptEvent.userChoice;
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    document.getElementById('butInstall').classList.toggle('hidden', true)
+    getElementById('butInstall').addEventListener('click', async () => {
+        console.log('👍', 'butInstall-clicked');
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+          // The deferred prompt isn't available.
+          return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        const result = await promptEvent.userChoice;
+        console.log('👍', 'userChoice', result);
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+        // Hide the install button.
+        document.getElementById('butInstall').classList.toggle('hidden', true)
+    })
 })
