30780fbc1c9190dacdf8b91eae85fafa772b3ddf
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2023-04-23 12:22:13 -0500

    no install btn :(

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,11 @@ import './App.css'
 function App() {
   const [gameChoice, setGameChoice] = useState('')
 
-  const installHandler = async () => {
-    console.log('👍', 'butInstall-clicked');
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-      // The deferred prompt isn't available.
-      return;
-    }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
-    const result = await promptEvent.userChoice;
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    document.getElementById('butInstall').classList.toggle('hidden', true)
-  }
-
   return (
-    <div className="">
+    <div>
         {gameChoice == '' && <StepOne choice={setGameChoice} /> }
         {gameChoice == 'Dark Souls 3' && <DarkSoulsThree choice={setGameChoice} />}
-        <button id="butInstall" onClick={installHandler} type="button" className="btn absolute bottom-5 left-5 text-xs">Install</button>
+        <button id="butInstall" type="button" className="btn absolute bottom-5 left-5 text-xs hide">Install</button>
     </div>
   )
 }
